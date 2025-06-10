57c969766fd58d4b1664c1c52fbd4991b385722e
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-06-10 11:19:49 +0700

    revisi typo 'spcae' to 'space'

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
-#include <exception>
-//untuk obyek exception yang akan digunakan
-#include <array>
-//untuk obyek array yang akan kita gunakan
-using name spcae std;
+#include <exception> 
+#include <array>    
+using namespace std;
 
 int main()
 {
-  cout << "Awal Progam" << endl; //penanda 1:....
+    cout << "Awal Program" << endl; // penanda 1: awal program
+
     try {
         array<int, 3> data = { 10, 20, 30 };
-        // int data[3] = { 10, 20, 30 }
-        // pesan array integer dengan 3 elemen
+        // array integer dengan 3 elemen
 
         cout << data.at(5) << endl;
-        // memanggil array dengan elemen ke 5
+        // mencoba mengakses elemen ke-5 (indeks 5), akan memicu exception
     }
     catch (exception& e) {
-        // penangkapan menggunakan objek exception
+        // menangkap exception menggunakan objek exception
         cout << e.what() << endl;
-        // akan dieksekusi karna array data hanya memiliki 3 elemen*/
+        // akan dieksekusi karena array hanya memiliki 3 elemen
     }
-    cout <<"Baris progam yang terakhir" << endl;
-    /*penanda 2 : bahwa progam berjalan berjalan tanpa berhenti meskipun terjadi  kesalahan*/
+
+    cout << "Baris program yang terakhir" << endl;
+    // penanda 2: program tetap berjalan meskipun terjadi kesalahan
+
     return 0;
-  }
+}
