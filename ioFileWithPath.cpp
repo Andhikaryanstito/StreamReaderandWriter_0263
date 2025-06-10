2627da6aef7898a596cdca6570d8f760dfb3819c
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-06-10 10:43:20 +0700

    Hentikan input saat pengguna mengetik 'q'

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -22,7 +22,10 @@ int main(){
         cout << "-";
     //mendapatkan setiap karakter dalam satu baris
     getline (cin, baris);
-
+    //loop akan berhenti jika anda memasukan karakter q
+    if (baris == "q") break;
+    //menulis dan memasukkan nilai dari 'baris' ke dalam file
+    outfile << baris <<endl;
     }
 
 
